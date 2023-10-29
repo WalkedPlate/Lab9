51ebd94a2eba63cd75b973b7a0bd83a94b8c1ba9
Al4cr4n <148933724+Al4cr4n@users.noreply.github.com>
2023-10-29 12:00:00 -0500

    Update TitleDao.java

--- src/main/java/com/example/clase10crud/daos/TitleDao.java
+++ src/main/java/com/example/clase10crud/daos/TitleDao.java
@@ -31,10 +31,10 @@ public class TitleDao {
 
             while (rs.next()) {
                 Title title = new Title();
-                title.setEmpNo(rs.getInt(1));
-                title.setTitle(rs.getString(2));
-                title.setFromDate(rs.getString(3));
-                title.setToDate(rs.getString(4));
+                title.setEmpNo(rs.getInt(2));
+                title.setTitle(rs.getString(3));
+                title.setFromDate(rs.getString(4));
+                title.setToDate(rs.getString(5));
 
                 lista.add(title);
             }
@@ -44,5 +44,102 @@ public class TitleDao {
         }
 
         return lista;
+    }
+    public void crear(int titleId, int titleEmpNo, String titleTtitle, String titleFromDate, String titleToDate){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/employees";
+        String username = "root";
+        String password = "root";
+
+        String sql = "insert into titles (id, emp_no, title, from_date, to_date) values (?,?,?,?,?)";
+
+        try(Connection connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+
+            pstmt.setInt(1,titleId);
+            pstmt.setInt(2,titleEmpNo);
+            pstmt.setString(3,titleTtitle);
+            pstmt.setString(4,titleFromDate);
+            pstmt.setString(5,titleToDate);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Title buscarPorId(String id){
+
+        Title title = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/employees";
+        String username = "root";
+        String password = "root";
+
+        String sql = "select * from titles where id = ?";
+
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1,id);
+
+            try(ResultSet rs = pstmt.executeQuery()){
+                while (rs.next()) {
+                    title = new Title();
+                    title.setId(rs.getInt(1));
+                    title.setEmpNo(rs.getInt(2));
+                    title.setTitle(rs.getString(3));
+                    title.setFromDate(rs.getString(4));
+                    title.setToDate(rs.getString(5));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return title;
+    }
+    public void actualizar(Title title){
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/employees";
+        String username = "root";
+        String password = "root";
+
+        String sql = "update titles set emp_no = ?, title = ?, from_date = ?, to_date = ? where id = ?";
+
+        try(Connection connection = DriverManager.getConnection(url,username,password);
+            PreparedStatement pstmt = connection.prepareStatement(sql)){
+
+            pstmt.setInt(1,title.getEmpNo());
+            pstmt.setString(2,title.getTitle());
+            pstmt.setString(3,title.getFromDate());
+            pstmt.setString(4,title.getToDate());
+            pstmt.setInt(1,title.getId());
+
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
